732e245dd5601dc9151e7f7f87b9307cda92c9b6
IcaroMatheusz <icaromatheus741@gmail.com>
2026-08-10 22:38:53 -0300

    explorando o flutter e entendendo como funciona a estruturação da página, foi criado uma página de bem-vindo simples

--- lib/main.dart
+++ lib/main.dart
@@ -5,25 +5,49 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-    const MyApp({super.key});
+  const MyApp({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('HelpDesk'), //titulo da pagina
-          ),
-          body: const Center (
-            child: Text( 
-              'Olá, técnico', //texto central
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [const Text('HelpDesk Mobile')],
+          ), //titulo da pagina, tentei deixar ele centralizado no meio
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment
+                .center, //anotar sobre isso, n faco ideia oq é? alinhamento do eixo principal?? acho que é pra deixar a coluna alinhada
+            children: [
+              const Text(
+                'Bem vindo, Técnico',
+                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
               ),
-            ),
+
+              const SizedBox(height: 20), //funciona a margin
+
+              const Text(
+                // outro texto
+                'Chamados em aberto: 5',
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(child: const Text('🟢 2')),
+
+                  const SizedBox(width: 20),
+
+                  Container(child: const Text('🔴 5')),
+                ],
+              ),
+            ],
           ),
-        )
-      );
-    }
+        ),
+      ),
+    );
+  }
 }
