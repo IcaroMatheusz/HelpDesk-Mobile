822a9605b8bde6178c5344684c8b1aecc14ca33a
IcaroMatheusz <icaromatheus741@gmail.com>
2026-08-11 23:43:18 -0300

    teste de renderização de dados de um array usando listview.builder, além disso foi alterado a estrutura da tela para dar scroll

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,24 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final chamados = const [
+    {
+      'titulo': 'Impressora com erro',
+      'empresa': 'Empresa X',
+      'status': StatusChamado.aberto,
+    },
+    {
+      'titulo': 'Sistema com problemas no acesso',
+      'empresa': 'Empresa Y',
+      'status': StatusChamado.resolvido,
+    },
+    {
+      'titulo': 'Erro no cadastro de produto',
+      'empresa': 'Empresa Z',
+      'status': StatusChamado.atendimento,
+    },
+  ];
+
   const MyApp({super.key});
 
   @override
@@ -18,84 +36,83 @@ class MyApp extends StatelessWidget {
             children: [const Text('HelpDesk Mobile')],
           ), //titulo da pagina, tentei deixar ele centralizado no meio
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment
-                .center, //anotar sobre isso, n faco ideia oq é? alinhamento do eixo principal?? acho que é pra deixar a coluna alinhada
-            children: [
-              const Text(
-                'Bem vindo, Técnico',
-                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-              ),
-
-              const SizedBox(height: 20), //funciona a margin
-
-              const Text('Chamados em aberto: 5'),
-
-              const SizedBox(height: 20), //funciona a margin
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                    child: Container(
-                      height: 80,
-                      padding: EdgeInsets.all(5),
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(color: Colors.blueGrey),
-                      child: const Text(
-                        'FINALIZADOS: 2',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-
-                  const SizedBox(width: 20),
-
-                  Expanded(
-                    child: Container(
-                      height: 80,
-                      padding: EdgeInsets.all(5),
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(color: Colors.blueGrey),
-                      child: const Text(
-                        'NÃO FINALIZADOS: 5',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-
-              const SizedBox(height: 20), //funciona a margin
-
-              const Text(
-                'ÚLTIMOS CHAMADOS',
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blueGrey,
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment
+                  .center, //anotar sobre isso, n faco ideia oq é? alinhamento do eixo principal?? acho que é pra deixar a coluna alinhada
+              children: [
+                const Text(
+                  'Bem vindo, Técnico',
+                  style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
                 ),
-              ),
 
-              Column(
-                children: [
-                  ChamadoCard(
-                    titulo: 'Impressora com erro',
-                    empresa: 'Empresa X',
-                    status: StatusChamado.aberto,
+                const SizedBox(height: 20), //funciona a margin
+
+                const Text('Chamados em aberto: 5'),
+
+                const SizedBox(height: 20), //funciona a margin
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 80,
+                        padding: EdgeInsets.all(5),
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(color: Colors.blueGrey),
+                        child: const Text(
+                          'FINALIZADOS: 2',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+
+                    const SizedBox(width: 20),
+
+                    Expanded(
+                      child: Container(
+                        height: 80,
+                        padding: EdgeInsets.all(5),
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(color: Colors.blueGrey),
+                        child: const Text(
+                          'NÃO FINALIZADOS: 5',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+
+                const SizedBox(height: 20), //funciona a margin
+
+                const Text(
+                  'ÚLTIMOS CHAMADOS',
+                  style: TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueGrey,
                   ),
+                ),
 
-                  const SizedBox(height: 20), //funciona a margin
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: chamados.length,
+                  itemBuilder: (context, index) {
+                    final chamado = chamados[index];
 
-                  ChamadoCard(
-                    titulo: 'Sistema com problemas no acesso',
-                    empresa: 'Empresa Y',
-                    status: StatusChamado.resolvido,
-                  ),
-                ],
-              ),
-            ],
+                    return ChamadoCard(
+                      titulo: chamado['titulo'] as String,
+                      empresa: chamado['empresa'] as String,
+                      status: chamado['status'] as StatusChamado,
+                    );
+                  },
+                ),
+              ],
+            ),
           ),
         ),
       ),
